bac552c058eb43fec66735e4aed5ee8c61221b5f
Gian Franco Peralta <gianfranco.peralta@alarcongroup.us>
2023-04-14 10:36:30 -0500

    refactor: home projects ui change ui tech list

--- app/components/molecules/ProjectCard.tsx
+++ app/components/molecules/ProjectCard.tsx
@@ -13,25 +13,27 @@ interface ProjectCardProps {
 export const ProjectCard = ({ description, image, name, technologies, url, code }: ProjectCardProps) => {
    return (
       <article className="flex flex-col laptop:grid grid-cols-2 gap-4 laptop:gap-14">
-         <picture className="flex items-center justify-center">
+         <picture className="flex items-start justify-center">
             <Image src={image} width={1000} height={1000} alt={name} className="w-full" />
          </picture>
          <div>
             <h3 className="font-medium text-xl laptop:text-3xl desktop:text-4xl ">{name}</h3>
             <p className="text-white text-lg desktop:text-2xl laptop:mt-4">{description}</p>
-            <p className="font-medium mt-4 text-xl">Tecnologías usadas:</p>
-            <div className="flex gap-4 mt-4 ">
-               {technologies.map((tech) => (
-                  <picture key={tech} className="flex items-center justify-center w-7" title={tech.toUpperCase()}>
-                     <Image src={`/${tech}.svg`} width={150} height={150} alt={tech} />
-                  </picture>
-               ))}
+            <div className="flex gap-4">
+               <p className="font-medium mt-4 text-xl">Tecnologías usadas:</p>
+               <div className="flex gap-4 mt-4 ">
+                  {technologies.map((tech) => (
+                     <picture key={tech} className="flex items-center justify-center w-7" title={tech.toUpperCase()}>
+                        <Image src={`/${tech}.svg`} width={150} height={150} alt={tech} />
+                     </picture>
+                  ))}
+               </div>
             </div>
             <div className="flex gap-4 laptop:gap-6 mt-4 laptop:mt-6">
                <a
                   href={url}
                   target="_blank"
-                  className="flex gap-3 items-center bg-primary text-black font-medium py-2 laptop:py-3 px-4 laptop:px-6 laptop:text-lg rounded-lg hover:bg-white transition-all duration-75 ease-linear"
+                  className="flex gap-3 items-center bg-primary text-black font-medium py-2  px-4  desktop:text-lg rounded-lg hover:bg-white transition-all duration-75 ease-linear"
                >
                   Ver sitio
                   <span className="w-full flex-1">
@@ -42,7 +44,7 @@ export const ProjectCard = ({ description, image, name, technologies, url, code 
                   <a
                      href={code}
                      target="_blank"
-                     className="flex gap-3 items-center bg-primary text-black font-medium py-2 laptop:py-3 px-4 laptop:px-6 laptop:text-lg rounded-lg hover:bg-white transition-all duration-75 ease-linear"
+                     className="flex gap-3 items-center bg-primary text-black font-medium py-2  px-4  desktop:text-lg rounded-lg hover:bg-white transition-all duration-75 ease-linear"
                   >
                      Ver código
                      <span className="w-full flex-1">
--- app/components/organisms/HomeContact.tsx
+++ app/components/organisms/HomeContact.tsx
@@ -1,71 +1,68 @@
-import { useForm } from "@/hooks";
-import { Loader } from "../atoms/Loader";
+import { useForm } from '@/hooks'
+import { Loader } from '../atoms/Loader'
 
 export const HomeContact = () => {
-  const { handleSubmit, emailError, isLoading, response } = useForm();
+   const { handleSubmit, emailError, isLoading, response } = useForm()
 
-  return (
-    <section className="py-8 laptop:py-14" data-section="/contact">
-      <h2 className="text-3xl laptop:text-5xl font-semibold ">Contacto</h2>
-      <p className="text-xl laptop:text-2xl mt-2 laptop:mt-4">
-        Si estás interesado en contactarme no dudes en hacerlo!
-      </p>
-      <form
-        className="flex flex-col gap-5 mt-4 laptop:gap-8"
-        onSubmit={handleSubmit}
-      >
-        <div className="flex flex-col gap-5 laptop:grid grid-cols-2 laptop:gap-12">
-          <div className="flex flex-col gap-2">
-            <label className="text-xl">Nombres:</label>
-            <input
-              type="text"
-              placeholder="Cristiano Ronaldo"
-              id="name"
-              name="name"
-              className="text-gray-500 px-4 py-2 rounded-lg outline-none"
-            />
-          </div>
-          <div className="flex flex-col gap-2 ">
-            <label className="text-xl">Email:</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="cristianocr7@gmail.com"
-              name="email"
-              className="text-gray-500 px-4 py-2 rounded-lg outline-none"
-            />
-            {emailError && <p className="text-red-400">{emailError}</p>}
-          </div>
-        </div>
-        <div className="flex flex-col gap-2">
-          <label className="text-xl">Mensaje:</label>
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Me gustaría contactarte..."
-            className="text-gray-500 px-4 py-2 rounded-lg outline-none h-36"
-          ></textarea>
-        </div>
-        <div className="relative">
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="inline-block bg-primary text-black font-medium py-2 laptop:py-4 px-4 laptop:px-8 laptop:text-lg rounded-lg hover:bg-white transition-all duration-75 ease-linear "
-          >
-            Enviar
-          </button>
-          {isLoading && <Loader />}
-          {response && (
-            <p
-              className={`${
-                emailError.length > 0 ? "text-red-400" : "text-primary"
-              } mt-4 font-medium text-lg laptop:text-xl `}
-            >
-              {response}
-            </p>
-          )}
-        </div>
-      </form>
-    </section>
-  );
-};
+   return (
+      <section className="py-8 laptop:py-14" data-section="/contact">
+         <h2 className="text-2xl tablet:text-3xl laptop:text-4xl desktop:text-5xl font-semibold">Contacto</h2>
+         <p className="text-xl laptop:text-2xl mt-2 laptop:mt-4">
+            Si estás interesado en contactarme, ¡No dudes en hacerlo!
+         </p>
+         <form className="flex flex-col gap-5 mt-4 laptop:gap-8" onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-5 laptop:grid grid-cols-2 laptop:gap-12">
+               <div className="flex flex-col gap-2">
+                  <label className="text-xl">Nombre completo:</label>
+                  <input
+                     type="text"
+                     placeholder="Cristiano Ronaldo"
+                     id="name"
+                     name="name"
+                     className="text-gray-500 px-4 py-2 rounded-lg outline-none"
+                  />
+               </div>
+               <div className="flex flex-col gap-2 ">
+                  <label className="text-xl">Email:</label>
+                  <input
+                     type="email"
+                     id="email"
+                     placeholder="cristianocr7@gmail.com"
+                     name="email"
+                     className="text-gray-500 px-4 py-2 rounded-lg outline-none"
+                  />
+                  {emailError && <p className="text-red-400">{emailError}</p>}
+               </div>
+            </div>
+            <div className="flex flex-col gap-2">
+               <label className="text-xl">Mensaje:</label>
+               <textarea
+                  name="message"
+                  id="message"
+                  placeholder="Me gustaría contactarte..."
+                  className="text-gray-500 px-4 py-2 rounded-lg outline-none h-36"
+               ></textarea>
+            </div>
+            <div className="relative">
+               <button
+                  type="submit"
+                  disabled={isLoading}
+                  className="inline-block bg-primary text-black font-medium py-2 laptop:py-4 px-4 laptop:px-8 laptop:text-lg rounded-lg hover:bg-white transition-all duration-75 ease-linear "
+               >
+                  Enviar
+               </button>
+               {isLoading && <Loader />}
+               {response && (
+                  <p
+                     className={`${
+                        emailError.length > 0 ? 'text-red-400' : 'text-primary'
+                     } mt-4 font-medium text-lg laptop:text-xl `}
+                  >
+                     {response}
+                  </p>
+               )}
+            </div>
+         </form>
+      </section>
+   )
+}
