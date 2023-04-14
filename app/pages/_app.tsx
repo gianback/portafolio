b34e2bffc568dd5084b04add541ddf6e5a40d443
Gian Franco Peralta <gianfranco.peralta@alarcongroup.us>
2023-04-13 19:08:03 -0500

    fea(app) favicon was added

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,25 +1,24 @@
-import { Favicon } from "@/components/atoms";
-import { Layout } from "@/components/layout";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import Head from "next/head";
+import { Layout } from '@/components/layout'
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Gian Franco Peralta Bravo</title>
-        <meta
-          name="description"
-          content="Hola! Soy Gian Franco, amante de la tecnología"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="../public/vercel.svg" />
-        <Favicon />
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
-  );
+   return (
+      <>
+         <Head>
+            <title>Gian Franco Peralta Bravo</title>
+            <meta name="description" content="Hola! Soy Gian Franco, amante de la tecnología" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+         </Head>
+         <Layout>
+            <Component {...pageProps} />
+         </Layout>
+      </>
+   )
 }
