836cc7eb2d6ef405b3bf66bf10c299c4a374c307
Gian Franco <gianfraback@gmail.com>
2023-04-16 13:08:15 -0500

    refactor: responsive was updated

--- app/components/organisms/HomeAbout.tsx
+++ app/components/organisms/HomeAbout.tsx
@@ -5,12 +5,12 @@ import styles from "@/styles/HomeAbout.module.css";
 export const HomeAbout = () => {
   return (
     <section className={`${styles.HomeAbout}`} data-section="/">
-      <div>
+      <div className="tablet:flex justify-center flex-col items-center laptop:block">
         <div>
-          <h1 className="text-2xl tablet:text-5xl desktop:text-6xl  text-white font-bold mt-6">
+          <h1 className="text-2xl tablet:text-5xl desktop:text-6xl tablet:text-center laptop:text-left  text-white font-bold mt-6">
             Gian Franco Peralta Bravo
           </h1>
-          <p className="text-primary font-medium text-xl tabletxl:text-2xl desktop:text-4xl mt-4 max-w-[45ch]">
+          <p className="text-primary font-medium text-xl tabletxl:text-2xl desktop:text-4xl mt-4 max-w-[45ch] tablet:text-center laptop:text-left">
             Cuento con{" "}
             <span className="font-bold text-white">
               {" "}
