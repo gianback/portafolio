481f869872f841a04cbf32de9ff6838936076a8d
Gian Franco Peralta <gianfranco.peralta@alarcongroup.us>
2023-04-18 11:10:41 -0500

    refactor: paragraph home contact was replaced

--- app/components/organisms/HomeContact.tsx
+++ app/components/organisms/HomeContact.tsx
@@ -5,7 +5,7 @@ export const HomeContact = () => {
       <section className="py-8 laptop:py-14 laptop:w-1/2 laptop:mx-auto" data-section="/contact">
          <h2 className="text-3xl laptop:text-5xl font-semibold laptop:text-center ">Contacto</h2>
          <p className="text-xl laptop:text-2xl mt-2 laptop:mt-4 laptop:text-center">
-            Si estás interesado en contactarme,!No dudes en hacerlo!
+            Si estás interesado en contactarme,!no dudes en hacerlo!
          </p>
          <Form />
       </section>
