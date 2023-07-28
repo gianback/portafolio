01691ed63f2ddad38c00fe3244e685e51a162c59
Gian Franco <gianfraback@gmail.com>
2023-07-28 14:58:45 -0500

    refactor: remove chat project

--- app/database/projects.ts
+++ app/database/projects.ts
@@ -1,6 +1,5 @@
 export const projects = [
   {
-    id: 5,
     name: "Movie App",
     description:
       "Movie App es una innovadora plataforma que permite a los usuarios leer reseñas y opiniones de películas y series sin spoilers. Proporciona recomendaciones detalladas y confiables, garantizando que los usuarios puedan sumergirse en el mundo del cine y las series sin temor a revelaciones no deseadas.",
@@ -18,17 +17,6 @@ export const projects = [
     ],
   },
   {
-    id: 4,
-    name: "Chat Group",
-    description:
-      "Chat Group fue un proyecto elaborado en equipo, el cual es un aplicativo divertido y entretenido que te permite conectarte con personas alrededor del mundo en un ambiente social y amistoso. Ya sea que te guste hablar sobre tus intereses, hacer nuevas amistades o simplemente divertirte con personas que compartan tus mismas ideas, Chat Group es el lugar ideal para ti.",
-    image: "/chat-group.webp",
-    url: "https://chat-group-beta.vercel.app/",
-    code: "https://github.com/gianback/chat-group",
-    technologies: ["nextjs", "supabase", "typescript", "tailwindcss", "git"],
-  },
-  {
-    id: 3,
     name: "Chile Tacos",
     description:
       "Chile Tacos & Beer es un restaurante ubicado en California que se especialista en comida mexicana y cuyo menú contiene ingredientes especiales del Estado de Jalisco. Su objetivo es satisfacer a sus clientes con comida deliciosa, fresca, y de excelente calidad. Algo característico de Chile Tacos & Beer es que tienen un menú especial dos veces a la semana.",
@@ -37,7 +25,6 @@ export const projects = [
     technologies: ["nextjs", "typescript", "tailwindcss", "strapi"],
   },
   {
-    id: 2,
     name: "Jimenez Painting",
     description:
       "Jimenez Painting cuenta con más de 5 años de experiencia proporcionando soluciones de pintura y sirviendo con orgullo un radio de 35 millas alrededor de Modesto, California.",
@@ -46,7 +33,6 @@ export const projects = [
     technologies: ["typescript", "nextjs", "strapi", "tailwindcss"],
   },
   {
-    id: 1,
     name: "JustPavers Design",
     description:
       "Encuentra la mejor instalación de adoquines de concreto en California. Ofrece un servicio integral, desde la instalación de adoquines hasta la aplicación de sellador. Utilizan adoquines de concreto entrelazados de alta calidad.",
